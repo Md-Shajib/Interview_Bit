9e0bd1322aa4a58b7e8ed0add5ea8a5a681e08ae
Md-Shajib <shajib0607@gmail.com>
2025-06-15 21:58:51 +0600

    Form text dynamic

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,16 +15,17 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import Image from 'next/image';
+import Link from 'next/link';
 
 const formSchema = z.object({
-  useername: z.string().min(2, "Username is required").max(20, "Username must be less than 20 characters"),
+  username: z.string().min(2, "Username is required").max(20, "Username must be less than 20 characters"),
 })
 
 interface AuthFormProps {
   type?: "sign-in" | "sign-up";
 }
 
-const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
+const AuthForm = ({ type }: {type: AuthFormProps}) => {
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -40,6 +41,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
     console.log(values)
   }
 
+  const isSignIn = type === "sign-in";
   return (
     <div className='card-border lg:min-ww[566px]'>
       <div className='flex flex-col gap-6 py-14 px-10'>
@@ -47,28 +49,21 @@ const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
           <Image alt='Logo' src="/logo.png" height={32} width={38}/>
           <h2 className='text-primary-100'>VivaBit</h2>
         </div>
+        <h3>Practice job interview with AI</h3>
+      
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-full mt-4 form">
+            {!isSignIn && <p>Name</p>}
+            <p>Email</p>
+            <p>Password</p>
+            <Button className='btn' type="submit">{isSignIn ? 'Sign In':'Create an account'}</Button>
+          </form>
+        </Form>
+        <p className='text-center'>
+          {isSignIn ? "Don't have an account?" : "Already have an account?"}
+          <Link href={!isSignIn ? '/sign-in':'/sign-up'} className='font-bold text-user-primary ml-1'>{!isSignIn ? "Sign in":"Sign up"}</Link>
+        </p>
       </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
     </div>
   );
 }
