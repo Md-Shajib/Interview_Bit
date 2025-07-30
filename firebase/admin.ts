b90b45f9f47745b2d49322dff372d5c2c812fa41
Md-Shajib <shajib0607@gmail.com>
2025-07-31 00:11:30 +0600

    env update

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -9,9 +9,9 @@ function initFirebaseAdmin() {
   if (!apps.length) {
     initializeApp({
       credential: cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_API_KEY,
       }),
     });
   }
